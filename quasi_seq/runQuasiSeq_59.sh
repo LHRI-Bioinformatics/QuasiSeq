2b865945e9f207b66642554f1dd41b7bad34ad1b
Brad T Sherman <bsherman@mail.nih.gov>
2020-12-01 17:31:35 -0500

    Added prompts for #processes,memory and output directory and removed old start scripts

--- quasi_seq/runQuasiSeq_59.sh
+++
@@ -1,9 +0,0 @@
-data_dir=/mnt/LHRI/Bioinformatics/Projects/Quasispecies/Docker_runs/multi59
-mkdir -p $data_dir
-
-docker run --cpus 4 --memory 16g --rm -i -t -v $data_dir:/data -v $(pwd):/opt/quasi-seq -p 8080:9000 lhri/lhri_bioinformatics/quasi-seq:1.3
-
-#in docker container:
-	#1) run /data/init.sh
-
-	 
--- runQuasiSeq.sh
+++ runQuasiSeq.sh
@@ -1,6 +1,26 @@
-ncores=$(getconf _NPROCESSORS_ONLN); njobs=$(( ncores > 1 ? (ncores - 1) : 1 ))
+#!/bin/bash
+
+#Prompt for the number of processes to use
+ncores=$(getconf _NPROCESSORS_ONLN);
+read -p "Enter the number of processes [${ncores}]: " ncoresPrompted
+ncoresPrompted=${ncoresPrompted:-${ncores}}
+ncores=${ncoresPrompted}
+
+njobs=$(( ncores > 1 ? (ncores - 1) : 1 ))
+
+#Prompt for the amount of memory to use in GB
+memFree=$(grep MemFree /proc/meminfo | awk '{print $2}')
+memFree=$((memFree / 1024 / 1024))
+read -p "Enter the amount of memory to use in GB [${memFree} GB]: " memFreePrompted
+memFreePrompted=${memFreePrompted:-${memFree}}
+memFree=${memFreePrompted}
+
+#Prompt for the output path
+outputDir="$(pwd)/output"
+read -p "Enter the path where you would like logging and output to be saved [${outputDir}]: " outputDirPrompted
+outputDirPrompted=${outputDirPrompted:-${outputDir}}
+outputDir=${outputDirPrompted}
 
 cp ./quasi_seq/cluster_interface/quasiseq/tasksMulti.py ./quasi_seq/cluster_interface/quasiseq/tasks.py
 
-docker run --cpuset-cpus="0-${njobs}" --cpus "${ncores}" --memory 64g --env my_threads="${ncores}" --rm -i -t -v $(pwd)/output:/data -v $(pwd)/quasi_seq:/opt/quasi-seq -p 8080:9000 lhri/lhri_bioinformatics/quasi-seq:1.3 
-	 
+docker run --cpuset-cpus="0-${njobs}" --cpus "${ncores}" --memory "${memFree}"g --env "my_threads=${ncores}" --rm -i -t -v ${outputDir}:/data -v $(pwd)/quasi_seq:/opt/quasi-seq -p 8080:9000 lhri/lhri_bioinformatics/quasi-seq:1.3
